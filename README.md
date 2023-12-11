e0263bb046a71944293aa0680c2f55aa22bb7b27
Praveen8161 <rbalu8161@gmail.com>
2023-12-11 23:29:22 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Forgot Password
+- [Live Site](https://kaleidoscopic-crepe-c6c530.netlify.app/)
---
+++ src/helpers/API.js
@@ -0,0 +1,5 @@
+// Server side URL
+export const API = 'http://localhost:9000' ;
+
+// client side URL for new password
+export const AppLink = 'http://localhost:5173' ;
